3f61291bc563b8dc8fae7b29801a4db69c6406e0
Jeevan Vetteel <jeevanvetteel@gmail.com>
2026-06-02 16:31:13 +1000

    Implemented naive SMA algo

--- sma.py
+++ sma.py
@@ -1,20 +1,23 @@
-def sma(prices: list, n: int):
+def sma_naive(prices: list, n: int):
     #prices = list of prices chronologically ordered
-    #n = number of days to go back
+    #n = number of days to go back (window length)
 
-    total = 0
     moving_avg = []
 
-    for i in range(n):
-        total += prices[-i-1]
-        moving_avg.append(total/(i+1))
-
+    for i in range(len(prices)):
+        if (i+1) >= n:
+            temp_total = 0
+            for j in range(n):
+                temp_total += prices[i-j]
+            moving_avg.append(temp_total/n)
+        elif (i+1) < n:
+            moving_avg.append(None)
 
     return moving_avg
 
 
-dummy = [1,2,3,4,5,6,7,8,9,10]
-days_interested = 10
+dummy = [1,2,3]
+days_interested = 0
 
 
-print(f"The simple moving average is: {sma(dummy,days_interested)}")
+print(f"The simple moving average is: {sma_naive(dummy,days_interested)}")
