5bc2a57006da10e37fb347ef9f1c938de2888f7c
Jeevan Vetteel <jeevanvetteel@gmail.com>
2026-08-28 16:11:07 +1000

    Added heatmap for SMA sweep analysis

--- sma_sweep.py
+++ sma_sweep.py
@@ -1,11 +1,11 @@
 from sma_strategy import compare_two_sma
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #create sweep range for multiple opportunities
 #short window: 5 to 50, by 5
 #long window: 10 to 200, by 10
-
-#compare_two_sma(stock, start,end, a: int, b: int)
 
 def sma_sweep(stock, start, end):
 
@@ -24,8 +24,9 @@ def sma_sweep(stock, start, end):
 
                 df.loc[i, j] = trading_value
 
-    print(df)
+    heatmap = sns.heatmap(df, vmin = indexed_value, cmap= "RdYlGn", annot = True)
+    plt.title(f"The indexed return (no active trading) is {round(indexed_value, 2)} point/s")
+    plt.show()
 
 
-
-#sma_sweep('IVV.AX','2020-01-01', '2026-01-01')
+#sma_sweep('IVV.AX','2025-01-01', '2026-01-01')
